ea839b1c445ad9555f491ae76495e031c44567bf
selvasonia <selvasonia@hotmail.fr>
2018-11-20 17:30:10 +0100

    fixturesregions1

--- src/DataFixtures/RegionFixtures.php
+++ src/DataFixtures/RegionFixtures.php
@@ -9,11 +9,30 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class RegionFixtures extends Fixture
 {
+
+    public function chargercsvregion(){
+        //lire le fichier csv
+        $regions = array();
+        $row = 1;
+        if(($handle = fopen("C:\\laragon\\www\\geststats\\src\\Controller\\regions.csv", "r")) !== FALSE){
+            while ( ($data = fgetcsv($handle,1000, ",")) !== FALSE){
+                    $regions[$row] = array(
+                            "id" => $data[0],
+                            "code" => $data[1],
+                            "name" => $data[2],
+                            "slug" => $data[3]
+                    );
+                $row++;
+            }
+            fclose($handle);
+        }
+        var_dump($regions);
+        die();
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker=Factory::create("fr_FR");
-
-        //création des regions
 
             $region=new Region();
             $region->setNom();

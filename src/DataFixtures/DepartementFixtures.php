559ecfbdd96308ebef982ad47d7ddf81e51c8cfc
selvasonia <selvasonia@hotmail.fr>
2018-11-20 18:06:35 +0100

    fixturereg

--- src/DataFixtures/DepartementFixtures.php
+++ src/DataFixtures/DepartementFixtures.php
@@ -10,19 +10,8 @@ class DepartementFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
 
-        $row = 1;
-        if (($handle = fopen("test.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        echo "<p> $num champs à la ligne $row: <br /></p>\n";
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
-        }
-    }
-    fclose($handle);
-}
+
         
-        $manager->flush();
+        // $manager->flush();
     }
 }

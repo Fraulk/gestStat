19883622fbd4136088316cb25ee91c80b260c58e
tunsay <40166667+tunsay@users.noreply.github.com>
2018-11-26 13:50:40 +0100

    tuna visiteur

---
+++ src/DataFixtures/VisiteurFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class VisiteurFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker=Factory::create("fr_FR");
+
+        $manager->flush();
+    }
+}

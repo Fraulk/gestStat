d2407b364a837504c7651a894b1211bf493ea7be
tunsay <40166667+tunsay@users.noreply.github.com>
2018-11-26 16:00:50 +0100

    tuna

--- src/Controller/StatController.php
+++ src/Controller/StatController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Faker\Factory;
 use App\Entity\Region;
+use App\Repository\RegionRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,8 +16,21 @@ class StatController extends AbstractController
      */
     public function home()
     {
+        
         return $this->render('stat/index.html.twig', [
             'controller_name' => 'StatController',
+        ]);
+    }
+
+    /**
+     * @Route("//liste", name="liste_region")
+     */
+    public function index(RegionRepository $repo)
+    {
+        $Regions=$repo->findAll();
+        return $this->render('Region/liste.html.twig', [
+            'controller_name' => 'RegionController',
+            'Regions' => $Regions
         ]);
     }
 

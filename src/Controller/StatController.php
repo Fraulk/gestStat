50f3903eec483cab8ba57ffb01b7a301542332f4
selvasonia <selvasonia@hotmail.fr>
2018-11-26 16:44:26 +0100

    vistreg

--- src/Controller/StatController.php
+++ src/Controller/StatController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Faker\Factory;
 use App\Entity\Region;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,5 +20,17 @@ class StatController extends AbstractController
             'controller_name' => 'StatController',
         ]);
     }
+
+    /**
+     * @Route("/visitr_reg", name="visiteursparregion")
+     */
+    public function visiteurparregion(Request $request) : Response
+    {
+        $search = new VisiteurParRegion;
+        $form = $this->createForm();
+        
+  
+    }
+
 
 }

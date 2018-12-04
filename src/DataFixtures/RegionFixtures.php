8c4f7641451e98b253a9535ea21f670e856f3304
Fraulk <fratulker@gmail.com>
2018-12-04 15:13:21 +0100

    fixFixtures

--- src/DataFixtures/RegionFixtures.php
+++ src/DataFixtures/RegionFixtures.php
@@ -4,6 +4,9 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Region;
+use App\Entity\Secteur;
+use App\Entity\Visiteur;
+use App\Entity\Departement;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -41,16 +44,70 @@ class RegionFixtures extends Fixture
         $faker=Factory::create("fr_FR");
 
             $regions = $this->chargercsvregion();
+
             // echo "<pre>";
             // var_dump($regions);
             // echo "</pre>";
             // die();
+                $dep=new Departement();
+                $dep->setDepCode("s")
+                    ->setDepNom( "Swiss");
+                $manager->persist($dep);
+                $dep1=new Departement();
+                $dep1->setDepCode("b")
+                     ->setDepNom( "Bourdon");
+                $manager->persist($dep1);
+                $dep2=new Departement();
+                $dep2->setDepCode("a")
+                    ->setDepNom( "Autre");
+                $manager->persist($dep2);
+                $depTable = [$dep, $dep1, $dep2];
+
+                $sec = new Secteur();
+                $sec->setSecCode("Nord")
+                    ->setSecLibelle("n");
+                $manager->persist($sec);
+                $sec1 = new Secteur();
+                $sec1->setSecCode("Ouest")
+                     ->setSecLibelle("o");
+                $manager->persist($sec1);
+                $sec2 = new Secteur();
+                $sec2->setSecCode("Sud")
+                     ->setSecLibelle("s");
+                $manager->persist($sec2);
+                $sec3 = new Secteur();
+                $sec3->setSecCode("Est")
+                     ->setSecLibelle("e");
+                $manager->persist($sec3);
+                $sec4 = new Secteur();
+                $sec4->setSecCode("Paris Centre")
+                     ->setSecLibelle("pc");
+                $manager->persist($sec4);
+                $secTable = [$sec, $sec1, $sec2, $sec3, $sec4];
+                
             foreach ($regions  as $key => $value) {
+
+
                 $region=new Region();
                 $region->setRegCode($value["code"]);
                 $region->setRegNom($value["name"]);
                 $manager->persist($region);
                 // var_dump($value);
+                for ($i=0; $i < mt_rand(3,8) ; $i++) { 
+                    $visitor = new Visiteur();
+
+                    $randSec = mt_rand(0,4);
+                    $randDep = mt_rand(0,2);
+                    $visitor->setVisMatricule(mt_rand(1000,9999))
+                            ->setVisNom($faker->name())
+                            ->setVisAdresse($faker->streetaddress())
+                            ->setVisCp($faker->postcode())
+                            ->setVisVille($faker->city())
+                            ->setVisDateembauche($faker->dateTimeBetween('-10 years', 'now'))
+                            ->setVisDep($depTable[$randDep])
+                            ->setVisSec($secTable[$randSec]);
+                    $manager->persist($visitor);
+                }
             }
         $manager->flush();
 
